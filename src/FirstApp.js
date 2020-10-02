21df7734a6ff67ac5a645b775994f5bbb50897d8
Iván Sánchez <ivansancrodr@gmail.com>
2020-10-02 22:29:02 +0200

    imprimir variables y objetos

--- src/FirstApp.js
+++ src/FirstApp.js
@@ -2,10 +2,19 @@
 
 import React, { Fragment } from "react";
 
+const saludo = 'Hola mundo';
+const objeto = {
+    nombre: 'Pepe',
+    edad: 34
+}
+
+
 const FirstApp = () => {
   return (
     <>
-      <h1>Hola mundo</h1>
+      <h1>{saludo}</h1>
+      <p> {JSON.stringify(objeto)}</p>
+      <pre> {JSON.stringify(objeto, null, 3)}</pre>
       <p> First App</p>
     </>
   );
