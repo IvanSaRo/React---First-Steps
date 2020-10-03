835df70920aab87d6d03af34dd3026b579d98b6c
Iván Sánchez <ivansancrodr@gmail.com>
2020-10-03 18:53:32 +0200

    dafaultprops

--- src/FirstApp.js
+++ src/FirstApp.js
@@ -7,24 +7,28 @@ const saludo = 'Hola mundo';
 const objeto = {
     nombre: 'Pepe',
     edad: 34
-}
+} 
 
 
-const FirstApp = ( {saludo1 /* = 'Hola gente' */} ) => {
+const FirstApp = ( {saludo1, subtitle}  /* = 'Soy un subtitulo' */ ) => {
 
     
   return (
     <>
       <h1>{saludo}</h1>
-      <h2>{ saludo1 }</h2>
+      <h2> {saludo1} </h2>
       <p> {JSON.stringify(objeto)}</p>
       <pre> {JSON.stringify(objeto, null, 3)}</pre>
-      <p> First App</p>
+      <p> {subtitle}</p>
     </>
   );
 };
 
-FirstApp.propTypes = {
+ FirstApp.propTypes = {
     saludo: PropTypes.string.isRequired
+} 
+
+FirstApp.defaultProps = {
+    subtitle: 'Soy un subtitulo'
 }
 export default FirstApp;
