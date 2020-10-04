054b4805aa23de4a4d4084b2e2e19e6b4281543c
Iván Sánchez <ivansancrodr@gmail.com>
2020-10-04 17:44:48 +0200

    eventos, pasar funciones vs pasar referencias

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -3,10 +3,20 @@ import PropTypes from 'prop-types';
 
 
 const CounterApp = ({value}) => {
-  return (
+  const handleAdd = (e) => {
+    console.log(e)
+  }
+  
+  
+  
+    return (
     <>
       <h1>CounterApp</h1>
       <h2> {value} </h2>
+
+      <button onClick={ handleAdd }>+1</button>{/* aqui no ejecutamos la función como tal, en ese caso se ejecutaría de forma secuencial,
+      ponemos la referencia para que se ejecute con el evento, si ejecutamos la función éstga ha de tener un return
+      que devuelva otra función */}
     </>
   );
 };
