f05d5c1fef319f6a1883194bfe894525c242aa1d
Iván Sánchez <ivansancrodr@gmail.com>
2020-10-04 18:06:35 +0200

    Introducción a hooks, variables y setters

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,29 +1,32 @@
-import React from "react";
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
+const CounterApp = ({ value }) => {
+  const [ counter, setCounter ] = useState(0);
 
-const CounterApp = ({value}) => {
-  const handleAdd = (e) => {
-    console.log(e)
-  }
   
   
-  
-    return (
+  const handleAdd = () => {
+    setCounter( counter + 1);
+    // si no tenemos acceso a la primera var(counter) podemos operar así con el set
+    // setCounter( () => c + 1)
+  };
+
+  return (
     <>
       <h1>CounterApp</h1>
-      <h2> {value} </h2>
+      <h2> {counter} </h2>
 
-      <button onClick={ handleAdd }>+1</button>{/* aqui no ejecutamos la función como tal, en ese caso se ejecutaría de forma secuencial,
+      <button onClick={handleAdd}>+1</button>
+      {/* aqui no ejecutamos la función como tal, en ese caso se ejecutaría de forma secuencial,
       ponemos la referencia para que se ejecute con el evento, si ejecutamos la función éstga ha de tener un return
       que devuelva otra función */}
     </>
   );
 };
 
-
 CounterApp.propTypes = {
-    value: PropTypes.number.isRequired
-} 
+  value: PropTypes.number.isRequired,
+};
 
 export default CounterApp;
