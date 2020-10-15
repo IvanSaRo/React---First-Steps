3d3582b9142766c22a7b9fd394aeac9526488c7b
Iván Sánchez <ivansancrodr@gmail.com>
2020-10-15 23:53:27 +0200

    pruebas sobre parte de un fragment

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,7 +2,7 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import {createSerializer} from 'enzyme-to-json';
+import {createSerializer} from 'enzyme-to-json';/* para los snapshots(junto con línea 9) */
 
 Enzyme.configure({ adapter: new Adapter() });
 
--- src/tests/FirstApp.test.js
+++ src/tests/FirstApp.test.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React from "react";
 import "@testing-library/jest-dom";
 import { shallow } from "enzyme";
 import FirstApp from "../FirstApp";
 
+describe("Pruebas en FirstApp", () => {
+  test("Debe mostrar FirstApp correctamente", () => {
+    const saludo = "Hola, soy Iván";
+    const wrapper = shallow(<FirstApp saludo={saludo} />);
 
+    expect(wrapper).toMatchSnapshot(); /*  */
+  });
 
-
-
-
-describe('Pruebas en FirstApp', () => {
-
-
-    test('Debe mostrar FirstApp correctamente', () => {
-        
-        const saludo = "Hola, soy Iván";
-        
-        const wrapper = shallow(<FirstApp saludo={saludo} />)
-
-        expect( wrapper ).toMatchSnapshot();/*  */
-    })
+  test('Debe mostrar el subtitulo enviado por props', () => {
+    const saludo = "Hola, soy Iván";
+    const subTitle = "Soy un subtitulo";
     
-})
+    const wrapper = shallow(
+        <FirstApp 
+            saludo={saludo} 
+            subtitle= {subTitle}
+        />
+    );
+    
+    const textoParrafo = wrapper.find('p').text();
+
+    expect(textoParrafo).toBe(subTitle);
+  })
+  
+});
